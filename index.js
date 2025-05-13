caaf52ea6f6cacdb45f6fdbcea75d436e3dc92f1
suneela-v <suneela.vankayala@gmail.com>
2025-05-13 14:37:52 +0530

    middle

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-
-// index.js
-
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+
 console.log("App is starting...");
 
 app.get('/', (req, res) => {
   res.send('Hello from Railway Express App!');
 });
+
 app.use((req, res, next) => {
   res.setHeader('Connection', 'keep-alive');
   next();
 });
 
-
-app.listen(8080, '0.0.0.0', () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Health check route
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
-setInterval(() => {}, 1000); // This will keep the process alive
 
-
-//redeploy purpose12
+// Start the server
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
+});
