09503fd4a1d064a0e45e506565a9fd39351892eb
suneela-v <suneela.vankayala@gmail.com>
2025-05-13 12:09:34 +0530

    asd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 console.log("App is starting...");
 
 app.get('/', (req, res) => {
